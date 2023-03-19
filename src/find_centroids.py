419899374779cf1d57a99b641300673f4e2aba97
Philip Mathieu <philip.eng.mathieu@gmail.com>
2023-03-19 18:13:30 -0400

    Move centroid script to python file

---
+++ src/find_centroids.py
@@ -0,0 +1,28 @@
+import argparse
+import pathlib
+import geopandas as gpd
+import osmnx as ox
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--graph", help="graph to use for search", default="data/maine.graphml", type=pathlib.Path)
+parser.add_argument("input", help="GeoPandas-compatible file to add centroids to", type=pathlib.Path)
+parser.add_argument("output_suffix", help="output shapefile suffix", default="_with_nodes", type=pathlib.Path)
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    # load graph and create geodataframe with nodes
+    G = ox.load_graphml(args.graph)
+    G = ox.project_graph(G, "EPSG:3857")
+    gdf_nodes = ox.graph_to_gdfs(G, edges=False).reset_index()
+
+    # load input file and drop invalid geometries
+    polys = gpd.read_file(input).to_crs("EPSG:3857")
+    raw_rows = polys.shape[0]
+    polys.dropna(subset="geometry", inplace=True)
+    n_dropped = raw_rows - polys.shape[0]
+    if n_dropped > 0:
+        print("Dropped {} rows with invalid geometries.".format(n_dropped))
+
+    # find nearest nodes using spatial index
+    polys["osmid"] = gdf_nodes.loc[gdf_nodes.sindex.nearest(polys.centroid)[1]]["osmid"].values
+    polys.to_file("data/tl_2022_23_tract_with_nodes"+args.output_suffix)
