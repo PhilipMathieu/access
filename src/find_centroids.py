6cd847ca114b95cbb506b0def2bb73cf6d38f221
Philip Mathieu <philip.eng.mathieu@gmail.com>
2023-03-24 11:26:09 -0400

    Update centroid script

--- src/find_centroids.py
+++ src/find_centroids.py
@@ -6,8 +6,7 @@ import osmnx as ox
 parser = argparse.ArgumentParser()
 parser.add_argument("-g", "--graph", help="graph to use for search", default="data/maine.graphml", type=pathlib.Path)
 parser.add_argument("input", help="GeoPandas-compatible file to add centroids to", type=pathlib.Path)
-parser.add_argument("output_suffix", help="output shapefile suffix", default="_with_nodes", type=pathlib.Path)
-parser.add_argument("-z", "--zip", help="zip the output shapefile when finished", default=False, action="store_true")
+parser.add_argument("-o", "--suffix", help="output shapefile suffix", default="_with_nodes", type=str)
 args = parser.parse_args()
 
 if __name__ == "__main__":
@@ -15,9 +14,13 @@ if __name__ == "__main__":
     G = ox.load_graphml(args.graph)
     G = ox.project_graph(G, "EPSG:3857")
     gdf_nodes = ox.graph_to_gdfs(G, edges=False).reset_index()
+    print("Loaded", args.graph)
 
-    # load input file and drop invalid geometries
-    polys = gpd.read_file(input).to_crs("EPSG:3857")
+    # load input file
+    polys = gpd.read_file(args.input).to_crs("EPSG:3857")
+    print("Loaded", args.input)
+
+    # drop invalid geometries
     raw_rows = polys.shape[0]
     polys.dropna(subset="geometry", inplace=True)
     n_dropped = raw_rows - polys.shape[0]
@@ -26,9 +29,11 @@ if __name__ == "__main__":
 
     # find nearest nodes using spatial index
     polys["osmid"] = gdf_nodes.loc[gdf_nodes.sindex.nearest(polys.centroid)[1]]["osmid"].values
-    outfile = "data/tl_2022_23_tract_with_nodes"+args.output_suffix
-    polys.to_file(outfile)
 
-    if args.zip:
-        from shutil import make_archive
-        make_archive(outfile+".zip", "zip", outfile)
+    # save output
+    outfile = str(args.input)+args.suffix+".shp.zip"
+    print("Saving", outfile)
+    polys.to_file(outfile, driver="ESRI Shapefile")
+
+    exit(0)
+
