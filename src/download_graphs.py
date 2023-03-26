6bf81e9cdcfab9be97a48397df25c71c8b84c58b
Philip Mathieu <philip.eng.mathieu@gmail.com>
2023-03-26 15:03:36 -0400

    Created Colab version

---
+++ src/download_graphs.py
@@ -0,0 +1,15 @@
+import osmnx as ox
+ox.settings.cache_folder = "./cache/"
+ox.settings.log_console = True
+print("Using OSMnx version", ox.__version__)
+print("WARNING: This script requires >10GB RAM available")
+
+# download/model a network of driving routes for the state of Maine
+G = ox.graph_from_place({"state": "Maine"}, network_type="drive")
+filepath = "./data/graphs/maine_drive.graphml"
+ox.save_graphml(G, filepath)
+
+# download/model a network of walking routes for the state of Maine
+G = ox.graph_from_place({"state": "Maine"}, network_type="walk")
+filepath = "./data/graphs/maine_walk.graphml"
+ox.save_graphml(G, filepath)
