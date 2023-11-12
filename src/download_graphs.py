169b495ed78848888fbe481b421fc2b8064464b5
Philip Mathieu <philip.eng.mathieu@gmail.com>
2023-11-12 16:00:53 -0500

    Make graph download state-by-state

--- src/download_graphs.py
+++ src/download_graphs.py
@@ -1,25 +1,60 @@
+import argparse
 import osmnx as ox
 from tqdm.auto import tqdm
 
 from utils import setup_osmnx, STATES
 
-setup_osmnx()
-print("Using OSMnx version", ox.__version__)
-print("WARNING: This script requires ~16GB RAM and may take several hours to run.")
-print(
-    "See notebooks/download_graphs_colab.ipynb for a Google Colab version that"
-    "can be run in a high-RAM notebook instance."
-)
 
-# loop through the list of states
-for state in tqdm(STATES, desc="States"):
-    for network_type in tqdm(["walk", "drive"], leave=False, desc=f"{state['name']}"):
-        tqdm.write(f"Downloading {state['name']} {network_type} network...")
-        # download the street network
-        G = ox.graph_from_place(
-            state["name"], network_type=network_type, simplify=True, retain_all=True
-        )
-        # project the graph to UTM (zone calculated automatically) then plot it
-        G = ox.project_graph(G)
-        # save as graphml file
-        ox.save_graphml(G, filepath=f"data/OSM/{state['FIPS']}_{network_type}.graphml")
+def download_graph(state, network_type):
+    # download the street network
+    G = ox.graph_from_place(
+        state["name"], network_type=network_type, simplify=True, retain_all=True
+    )
+    # project the graph to UTM (zone calculated automatically) then plot it
+    G = ox.project_graph(G)
+    # save as graphml file
+    ox.save_graphml(G, filepath=f"data/OSM/{state['FIPS']}_{network_type}.graphml")
+
+
+def download_graphs(states, network_types):
+    setup_osmnx()
+    print("Using OSMnx version", ox.__version__)
+    print("WARNING: This script requires ~16GB RAM and may take several hours to run.")
+    print(
+        "See notebooks/download_graphs_colab.ipynb for a Google Colab version that"
+        "can be run in a high-RAM notebook instance."
+    )
+
+    # loop through the list of states
+    for state in tqdm(states, desc="States"):
+        for network_type in tqdm(
+            network_types, leave=False, desc=f"{state['name']}"
+        ):
+            tqdm.write(f"Downloading {state['name']} {network_type} network...")
+            download_graph(state, network_type)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Download street network graphs for specified states and network types')
+    parser.add_argument('--states', nargs='+', default=[state['name'] for state in STATES], help='List of state names to download')
+    parser.add_argument('--network-types', nargs='+', default=["walk", "drive"], help='List of network types to download')
+    args = parser.parse_args()
+
+    if len(args.states) == 0:
+        states = STATES
+    else:
+        states = []
+        for state in STATES:
+            if state['name'] in args.states:
+                states.append(state)
+            elif state['FIPS'] in args.states:
+                states.append(state)
+            else:
+                print(f"State {state['name']} not found in list of states.")
+
+    if len(args.network_types) == 0:
+        network_types = ["walk", "drive"]
+    else:
+        network_types = args.network_types
+    
+    download_graphs(states, network_types)
